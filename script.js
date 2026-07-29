0128c1ad2d1f71eb1f15bfe8a7a64cf5f8e6abd6
arianneaaa <aarianneandril@gmail.com>
2026-07-29 20:12:09 +0800

    add buttons

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 Score = 0;  
 
 const incrementBtn = document.querySelector('#increment');
+const decrementBtn = document.querySelector('#decrement');
+const resetBtn = document.querySelector('#reset');
 const display = document.getElementById('display');
 
 incrementBtn.addEventListener('click', () => {
     Score++;
+    display.textContent = `Score: ${Score}`;
+});
+
+decrementBtn.addEventListener('click', () => {
+    Score--;
+    display.textContent = `Score: ${Score}`;
+});
+
+resetBtn.addEventListener('click', () => {
+    Score=0;
     display.textContent = `Score: ${Score}`;
 });
