e8fae5f0f0884571d7da16034c82ea292235f063
an_arianne <aarianneandril@gmail.com>
2026-07-29 12:01:25 +0800

    add buttons

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 Score = 0
+
+const incrementBtn = document.querySelector('#increment');
+const display = document.getElementById('#display');
+
+incrementBtn.addEventListener('click', () => {
+    Score++;
+    display.textContent = `Score: ${score}`;
+})
